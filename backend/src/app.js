30f8b1aba7838f37fbd4a187c198c9af88c99eb2
thalesribeiropalha <thales.rp@hotmail.com>
2025-07-10 09:27:19 -0300

    fix: cors policy

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // Middlewares
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:5175',
-    'https://odonto-app-eight.vercel.app',
-    process.env.FRONTEND_URL
-  ].filter(Boolean),
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -61,6 +52,7 @@ app.use((req, res) => {
 });
 
 module.exports = app;
+
 
 
 
